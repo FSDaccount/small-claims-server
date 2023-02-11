a0fcfa258fed9ac8e126a9e875253754ebcfeec2
daLongBoi <duncan.kane1997@hotmail.com>
2023-02-11 14:17:57 +0000

    final

--- src/main/java/com/allstate/smallclaimsserver/service/BootstrapService.java
+++ src/main/java/com/allstate/smallclaimsserver/service/BootstrapService.java
@@ -7,16 +7,13 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 
-
-
 @Service
 public class BootstrapService {
     
     @Autowired
-    private ClaimRepository claimRepository;
+    private ClaimRepository ClaimRepository;
     @PostConstruct
     public void setupInitialData(){
-        Claim newClaim = new Claim(1, "firstName","surname", "example@email.com","Car","fully come",5000.00,"car crash","open");
-        claimRepository.save(newClaim);
+        Claim newClaim = new Claim(1, "firstName","surname", "example@email.com","Car","fully come",5000.00,"car crash","open",2023-01-02 );
     }
 }
